746b42764f20788a4c5d42acabe2317031b9cc7f
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-06-10 20:01:14 -0700

    Add a nicer api for registering formatters

--- lib/git/approvals/approval.rb
+++ lib/git/approvals/approval.rb
@@ -4,16 +4,48 @@ module Git
   module Approvals
     class Approval
 
-      FORMATTERS = {
-        :txt => lambda { |object|
-          require 'awesome_print'
-          object.awesome_inspect :plain => true, :indent => -2
-        },
-        :json => lambda { |string|
-          require 'json'
-          JSON.pretty_generate JSON.parse( string )
-        }
-      }
+      class << self
+
+        ##
+        # Registers a new formatter block by name. The block
+        # is expected to return a deterministic string
+        # representation of an object.
+        def register_formatter( name, &block )
+          formatters[ name.to_sym ] = block
+        end
+
+        ##
+        # Looks up the formatter named `name` and attempts to
+        # format `object`. Raises a helpful error message if
+        # a formatter's soft dependency cannot be loaded.
+        def format( name, object )
+          formatters[ name ][ object ]
+        end
+
+        protected
+
+        ##
+        # The hash of registered formatters by name.
+        def formatters
+          @formatters ||= { }
+        end
+      end
+
+      ##
+      # The `txt` format requires the `awesome_print` gem.
+      # It is suitable for formatting most native ruby types.
+      register_formatter :txt do |object|
+        require 'awesome_print'
+        object.awesome_inspect :plain => true, :indent => -2
+      end
+
+      ##
+      # The `json` format requires the `json` library.
+      # It is suitable for formatting JSON strings.
+      register_formatter :json do |object|
+        require 'json'
+        JSON.pretty_generate JSON.parse( object )
+      end
 
       def initialize( path, options={} ) # :nodoc:
         @path, @options = path, options
@@ -25,14 +57,17 @@ module Git
       attr_reader :path, :options
 
       ##
-      #
+      # Diffs the given string with this approval file. If the
+      # file has not been checked in, this method will raise an
+      # exception. Otherwise, the supplied block will only be
+      # called if the diff fails, meaning there are differences.
       def diff( string, &block )
         # Make sure the directory of the file exists.
         FileUtils.mkdir_p File.dirname( path )
 
         # Write the new string to the file.
         File.open path, 'w' do |f|
-          f << FORMATTERS[ format ][ string ]
+          f << self.class.format( format, string )
         end
 
         # If the file hasn't been checked in, raise an error.
